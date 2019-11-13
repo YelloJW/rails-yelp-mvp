f8e8517a2c41d2b9479a5371ab752bca4f1083e0
Joe Olley <joeolley@hotmail.co.uk>
2019-11-13 14:53:34 +0000

    CRUD actions for restaurant and review models with simple bootstrap styling

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,12 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  before_validation :category_downcase
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'],
     message: '%{value} is not a valid size' }
+
+  def category_downcase
+    self.category = self.category.downcase
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Hoxton 100', address: 'Hoxton', category: 'italian')
+Restaurant.create(name: 'Lambeth 200', address: 'Lambeth', category: 'french')
+Restaurant.create(name: 'Old street 300', address: 'Old street', category: 'belgian')
+Restaurant.create(name: 'Brixton 400', address: 'Brixton', category: 'chinese')
+Restaurant.create(name: 'Wandsworth 600', address: 'Wandsworth', category: 'french')
+Restaurant.create(name: 'Bethnal green 700', address: 'Bethnal green', category: 'italian')
+Restaurant.create(name: 'Clapham 800', address: 'Hoxton', category: 'belgian')
+Restaurant.create(name: 'Bow 900', address: 'Bow', category: 'chinese')
